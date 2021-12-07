9481d6cd5109a7363981aa7cfd3a7893c9ed4fb1
chocom01 <mukola0101@gmail.com>
2021-12-07 18:07:31 +0200

    Build relations for user and task

--- database/migrations/2021_12_07_151424_create_tasks_table.php
+++ database/migrations/2021_12_07_151424_create_tasks_table.php
@@ -16,6 +16,7 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
             $table->index('name');
         });
