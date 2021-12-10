c51c6292c964c1871d0e8b362e70c6776f3a6bc2
chocom01 <mukola0101@gmail.com>
2021-12-10 16:02:51 +0200

    Added timestamps false property and guarded 'id'

--- app/Models/Priority.php
+++ app/Models/Priority.php
@@ -4,14 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Priority extends Model
 {
     use HasFactory;
 
-    protected $guarded = [];
+    public $timestamps = false;
 
-    public function tasks()
+    protected $guarded = ['id'];
+
+    public function tasks(): HasMany
     {
         return $this->hasMany(Task::class);
     }
