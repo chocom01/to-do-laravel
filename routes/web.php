2391718b656d45a26d6bdba737a773a2b610a21d
chocom01 <mukola0101@gmail.com>
2021-12-10 15:42:40 +0200

    Changed all single routes to Route::resource

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,6 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{orderBy?}/{sortBy?}', [TaskController::class, 'index'])
-    ->where([
-        'orderBy' => '(name|user_id|status_id|priority_id)',
-        'sortBy' => '(asc|desc)'
-    ])->name('home');
-
-Route::get('/tasks', [TaskController::class, 'create']);
-Route::post('/tasks', [TaskController::class, 'store']);
-
-Route::get('/tasks/{task:id}', [TaskController::class, 'edit']);
-Route::patch('/tasks/{task:id}', [TaskController::class, 'update']);
-Route::delete('/tasks/{task:id}', [TaskController::class, 'destroy']);
+Route::resource('tasks', TaskController::class)
+    ->except(['show'])
+    ->names(['index' => 'home']);
