568fbba109d23046a71fe2b8a9c13593ea80431f
chocom01 <mukola0101@gmail.com>
2021-12-10 15:44:10 +0200

    Deleted useless Factories

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,12 +2,8 @@
 
 namespace Database\Factories;
 
-use App\Models\Priority;
-use App\Models\Status;
 use App\Models\Task;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class TaskFactory extends Factory
 {
@@ -23,9 +19,6 @@ class TaskFactory extends Factory
     {
         return [
             'name' => $this->faker->sentence(),
-            'user_id' => User::factory(),
-            'status_id' => Status::factory(),
-            'priority_id' => Priority::factory()
         ];
     }
 }
