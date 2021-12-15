c448c4c491e56d90e139f556e61ac4e7412f2928
chocom01 <mukola0101@gmail.com>
2021-12-15 12:09:39 +0200

    Added redirect to home route

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::resource('tasks', TaskController::class)
     ->except(['show'])
     ->names(['index' => 'home']);
+
+Route::get('/', function () {
+    return redirect()->home();
+});
