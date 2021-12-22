412333ce4a7d537045d4c136c228b8b2267d7949
chocom01 <mukola0101@gmail.com>
2021-12-22 18:22:42 +0200

    Default bcrypt password for seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,7 +16,8 @@ class UserSeeder extends Seeder
     {
         User::factory()->create([
             'name' => 'Kolya',
-            'email' => 'mukola0303@gmail.com'
+            'email' => 'mukola0303@gmail.com',
+            'password' => bcrypt('chocm31415')
         ]);
 
         User::factory()->create();
