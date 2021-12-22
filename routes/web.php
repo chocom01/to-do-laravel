9a12442b63d7e9b002ffd94effdfbfadaef53534
chocom01 <mukola0101@gmail.com>
2021-12-22 18:21:36 +0200

    Added middleware('auth') and route for auth

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,32 @@
 use App\Http\Controllers\Web\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('tasks', TaskController::class)
-    ->except(['show'])
-    ->names([
-        'index' => 'home',
-        'edit' => 'edit.task'
-    ]);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('/', function () {
-    return redirect()->home();
+Route::middleware('auth')->group(function () {
+    Route::resource('tasks', TaskController::class)
+        ->except(['show'])
+        ->names([
+            'index' => 'home',
+            'edit' => 'edit.task'
+        ]);
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/', function () {
+        return redirect()->home();
+    });
 });
+
+require __DIR__ . '/auth.php';
