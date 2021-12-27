0164a0423571b77ab48fc7a407dac4b925fa2275
chocom01 <mukola0101@gmail.com>
2021-12-27 14:51:43 +0200

    Added middleware auth and permission in routes for delete tasks

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,18 @@
 use App\Http\Controllers\Api\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('tasks', TaskController::class)
-    ->names([
-        'index' => 'home'
-    ]);
+//Route::apiResource('tasks', TaskController::class)
+//    ->names([
+//        'index' => 'home'
+//    ]);
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::apiResource('tasks', TaskController::class)
+        ->except(['destroy'])
+        ->names([
+            'index' => 'home'
+        ]);
+
+    Route::delete('api/tasks/{task}', [TaskController::class, 'destroy'])
+        ->middleware('permission:deleteTask');
+});
--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,16 @@
 use App\Http\Controllers\Web\TaskController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::middleware('auth')->group(function () {
+Route::group(['middleware' => ['auth']], function () {
     Route::resource('tasks', TaskController::class)
-        ->except(['show'])
+        ->except(['show', 'destroy'])
         ->names([
             'index' => 'home',
             'edit' => 'edit.task'
         ]);
+
+    Route::delete('tasks/{task}', [TaskController::class, 'destroy'])
+        ->middleware('permission:deleteTask');
 
     Route::get('/dashboard', function () {
         return view('dashboard');
