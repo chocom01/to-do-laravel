5cf5c032bfca73a3c22a9614841b8126c8fe1859
chocom01 <mukola0101@gmail.com>
2022-01-04 19:48:13 +0200

    Replace css styles connect to layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,8 @@
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <link href="{{ asset('/css/nav.css') }}" rel="stylesheet">
+<link href="{{ asset('/css/form.css') }}" rel="stylesheet">
+<link href="{{ asset('/css/tasks.index.css') }}" rel="stylesheet">
 
 <nav>
     <div class="topnav">
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,6 +1,4 @@
 <x-layout>
-    <link href="{{ asset('/css/form.css') }}" rel="stylesheet">
-
     <div class="center">
         <p class="text-2xl mb-6 flex justify-center"> Create task </p>
         <form method="POST" action="/tasks">
