06dd886b447ebb2fce265fca22851b8b28548c9b
chocom01 <mukola0101@gmail.com>
2022-01-18 10:33:41 +0200

    Deleted useless Aliasing

--- app/Http/Controllers/Web/TaskController.php
+++ app/Http/Controllers/Web/TaskController.php
@@ -5,16 +5,12 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\IndexTaskRequest;
 use App\Http\Requests\TaskValidationRequest;
-use App\Mail\UserActiveTasksMail;
 use App\Models\Priority;
 use App\Models\Status;
 use App\Models\Task;
 use App\Models\User;
 use App\Services\TaskService;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\View\View;
 
 class TaskController extends Controller
